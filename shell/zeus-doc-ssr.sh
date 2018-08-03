38e7d7a8bb0802a7b608ee4d5378380cdf59ae7b
qiaoming <qiaoming@zcool.com.cn>
2018-08-03 15:25:10 +0800

    修改部署

--- shell/zeus-doc-ssr.sh
+++ shell/zeus-doc-ssr.sh
@@ -12,7 +12,6 @@ echo "---------- start install devpendence ! ----------"
 yarn install
 
 echo "---------- start build ! ----------"
-yarn build
 pm2 restart ./server/prod.js
 
 if [ $? -ne 0 ]; then
